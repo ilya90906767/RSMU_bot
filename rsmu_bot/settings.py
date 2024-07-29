86bafd16d10c90fa5fb41ce3cda03d0c3ceae9bd
egorverkhozin <nonamers1336@gmail.com>
2024-07-29 13:39:30 +0300

    added CSRF_TRUSTED_ORIGINS

--- rsmu_bot/settings.py
+++ rsmu_bot/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 
+CSRF_TRUSTED_ORIGINS = ['https://*.nanoscript-land.ru/']
+
 ALLOWED_HOSTS = ['*']
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
